ce415ec4cd36e286cce742f693cd8090667dcff9
JohnTayse <JohnTayse@users.noreply.github.com>
2022-08-04 16:03:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 #! /Library/Frameworks/Python.framework/Versions/3.8/bin/python3.8
 # -*- coding: utf-8 -*-
 
-import os
-
-ACTIVE_LOG = os.environ['ACTIVE_LOG']
-if ACTIVE_LOG == None:
-	ACTIVE_LOG = False
+ACTIVE_LOG = False
